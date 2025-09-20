0e61e300e82ddd39cddc6006b9618bc18d273f9c
KesariIncture <kesarinandan.patro@incture.com>
2025-09-20 19:52:01 +0530

    ..

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,5 +1,5 @@
 import { TrendUpIcon } from "@phosphor-icons/react";
-import { Card, Typography } from "antd";
+import { Card, Typography, Row, Col } from "antd";
 import React from "react";
 
 const MainContent = () => {
@@ -15,110 +15,132 @@ const MainContent = () => {
     >
       <Text style={{ fontSize: "14px", fontWeight: "600" }}>eCommerce</Text>
 
-      <div
-        style={{
-          marginTop: "24px",
-          display: "grid",
-          gridTemplateColumns: "repeat(2, 200px)",
-          gridTemplateRows: "repeat(2, auto)",
-          gap: "16px",
-        }}
-      >
-        <Card
-          size="small"
-          style={{
-            width: 200,
-            backgroundColor: "#E4F5FF",
-            padding: "10px",
-            borderRadius: "20px",
-          }}
-        >
-          <Text style={{ fontSize: "14px", fontWeight: "600" }}>Customers</Text>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text style={{ fontSize: "25px", fontWeight: "600" }}>3,781</Text>
-            <Text>
-              +11.01 <TrendUpIcon />
-            </Text>
-          </div>
-        </Card>
+      <div style={{ marginTop: "24px", maxWidth: "400px" }}>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <Card
+              size="small"
+              style={{
+                width: "100%",
+                maxWidth: 200,
+                backgroundColor: "#E4F5FF",
+                padding: "10px",
+                borderRadius: "20px",
+              }}
+            >
+              <Text style={{ fontSize: "14px", fontWeight: "600" }}>
+                Customers
+              </Text>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={{ fontSize: "25px", fontWeight: "600" }}>
+                  3,781
+                </Text>
+                <Text>
+                  +11.01 <TrendUpIcon />
+                </Text>
+              </div>
+            </Card>
+          </Col>
 
-        <Card
-          size="small"
-          style={{
-            width: 200,
-            backgroundColor: "#f7f9f5",
-            padding: "10px",
-            borderRadius: "20px",
-          }}
-        >
-          <Text style={{ fontSize: "14px", fontWeight: "600" }}>Orders</Text>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text style={{ fontSize: "25px", fontWeight: "600" }}>1,219</Text>
-            <Text>
-              -0.03 <TrendUpIcon />
-            </Text>
-          </div>
-        </Card>
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <Card
+              size="small"
+              style={{
+                width: "100%",
+                maxWidth: 200,
+                backgroundColor: "#f7f9f5",
+                padding: "10px",
+                borderRadius: "20px",
+              }}
+            >
+              <Text style={{ fontSize: "14px", fontWeight: "600" }}>
+                Orders
+              </Text>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={{ fontSize: "25px", fontWeight: "600" }}>
+                  1,219
+                </Text>
+                <Text>
+                  -0.03 <TrendUpIcon />
+                </Text>
+              </div>
+            </Card>
+          </Col>
 
-        <Card
-          size="small"
-          style={{
-            width: 200,
-            backgroundColor: "#f7f9f5",
-            padding: "10px",
-            borderRadius: "20px",
-          }}
-        >
-          <Text style={{ fontSize: "14px", fontWeight: "600" }}>Revenue</Text>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text style={{ fontSize: "25px", fontWeight: "600" }}>$695</Text>
-            <Text>
-              +15.03 <TrendUpIcon />
-            </Text>
-          </div>
-        </Card>
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <Card
+              size="small"
+              style={{
+                width: "100%",
+                maxWidth: 200,
+                backgroundColor: "#f7f9f5",
+                padding: "10px",
+                borderRadius: "20px",
+              }}
+            >
+              <Text style={{ fontSize: "14px", fontWeight: "600" }}>
+                Revenue
+              </Text>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={{ fontSize: "25px", fontWeight: "600" }}>
+                  $695
+                </Text>
+                <Text>
+                  +15.03 <TrendUpIcon />
+                </Text>
+              </div>
+            </Card>
+          </Col>
 
-        <Card
-          size="small"
-          style={{
-            width: 200,
-            backgroundColor: "#E5ECF6",
-            padding: "10px",
-            borderRadius: "20px",
-          }}
-        >
-          <Text style={{ fontSize: "14px", fontWeight: "600" }}>Growth</Text>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text style={{ fontSize: "25px", fontWeight: "600" }}>30.1%</Text>
-            <Text>
-              +6.08 <TrendUpIcon />
-            </Text>
-          </div>
-        </Card>
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <Card
+              size="small"
+              style={{
+                width: "100%",
+                maxWidth: 200,
+                backgroundColor: "#E5ECF6",
+                padding: "10px",
+                borderRadius: "20px",
+              }}
+            >
+              <Text style={{ fontSize: "14px", fontWeight: "600" }}>
+                Growth
+              </Text>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={{ fontSize: "25px", fontWeight: "600" }}>
+                  30.1%
+                </Text>
+                <Text>
+                  +6.08 <TrendUpIcon />
+                </Text>
+              </div>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   );
